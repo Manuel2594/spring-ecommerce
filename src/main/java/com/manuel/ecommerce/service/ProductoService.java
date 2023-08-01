d3acf5195bf60613343033e549823bea9bcbdd92
Manuel2595 <wolf2367@gmail.com>
2023-08-01 15:02:34 -0400

    Funcionalidad mostrar productos para el administrador

--- src/main/java/com/manuel/ecommerce/service/ProductoService.java
+++ src/main/java/com/manuel/ecommerce/service/ProductoService.java
@@ -1,5 +1,6 @@
 package com.manuel.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -12,5 +13,6 @@ public interface ProductoService {
 	public Optional<Producto> get(Integer id);
 	public void update(Producto producto);
 	public void delete(Integer id);
+	public List<Producto> findAll();
 
 }
